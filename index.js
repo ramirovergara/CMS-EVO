8edac59e214a734956cecc0bf50418aaddb3466c
tony <>
2018-05-07 16:03:17 +0200

    change dir index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be use
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + 'index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
 // Start the app by listening on the default
 // Heroku portgit
